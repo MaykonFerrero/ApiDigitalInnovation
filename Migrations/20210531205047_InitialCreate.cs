c5d6c973722055467ab4e62a195c6eab3e6847ef
MaykonFerrero <maykon.h.ferrero@gmail.com>
2021-06-09 16:32:28 -0400

    primeiro commit

---
+++ Migrations/20210531205047_InitialCreate.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace CursoMVC.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up([NotNull]MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categorias",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation(name:"SqlServer:Identity", value:"1, 1"),
+                    Descrição = table.Column<string>( nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey(name: "PK_Categorias",columns: x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categorias");
+        }
+    }
+}
+
---
+++ Models/Context.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVC.Models
+{
+    public class Context : DbContext
+    {
+
+        public DbSet<Categoria> Categorias { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: @"Server = localhost\SQLEXPRESS; Database = master; Trusted_Connection = True");
+        }
+
+    }
+}
